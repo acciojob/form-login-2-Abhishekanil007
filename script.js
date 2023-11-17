a0af9fced9e2177af077e553ea6f7f20ece8cfb3
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-17 14:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function handleSubmit(event)
+	{
+		event.preventDefault();
+
+		var fname = document.getElementById('fname').value;
+		var lname = document.getElementById('lname').value;
+		var phone = document.getElementById('phone').value;
+		var email = document.getElementById('email').value;
+
+		alert('First name: ' + fname + '\nLast Name: ' + lname + '\nPhone Number: ' + phone + '\nEmail: ' + email);
+	}
+
+
+document.getElementById('myform').addEventListener('submit' , handleSubmit);
